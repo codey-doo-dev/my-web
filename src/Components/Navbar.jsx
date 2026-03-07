26e1e9a7dd680fc2008e48e77ff5137544a2bb2b
codey-doo-dev <m95730047@gmail.com>
2026-03-07 06:47:46 +0500

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+  <nav className=''>
+    <div className=''> myWeb</div>
+   <div className=''>
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+    </ul>
+   </div>
+   <div>
+    <button>Sign up</button>
+    <button>Sign in</button>
+   </div>
+</nav>
+   
+
+  )
+}
+
+export default Navbar
