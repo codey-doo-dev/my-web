63bf2dec0fd441117a1a290d1f448fc6e0cfaed4
codey-doo-dev <m95730047@gmail.com>
2026-03-10 10:32:44 +0500

    Card

---
+++ src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <h3>{Props.name}</h3>
+      <p>{Props.age}</p>
+      <p>{Props.role}</p>
+    </div>
+  )
+}
+
+export default Card
