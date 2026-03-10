d2ffab350cbefbcffec8c7f7a75feba3ca525513
codey-doo-dev <m95730047@gmail.com>
2026-03-10 11:03:33 +0500

    user data

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const User = () => {
   return (
     <div>
-      
+      {Users.map(
+        (User) => (
+         <div key={User.id}>
+              <h3>{User.name}</h3>
+              <p>{User.age}</p>
+              <p>{User.role}</p>
+            </div>
+        )
+      )}
     </div>
   )
 }
