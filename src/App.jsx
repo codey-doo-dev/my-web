381d62678d647ee7cb9915456969985249c829df
codey-doo-dev <m95730047@gmail.com>
2026-03-12 01:45:54 +0500

    delete

--- src/App.jsx
+++ src/App.jsx
@@ -5,65 +5,8 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Services from "./Pages/Services";
 import Contact from "./Pages/Contact";
-import { Button } from "./Components/Button";
-import {Card} from "./Components/Card";
-import { User } from "./Components/User";
-    const App = () => {
-  return (
-    <div>
-      <Button content="Sign Up" />
-      <Button content="Register" />
-      <Button content="Log in" />
-      <Button content="Sign In" />
-      <br/><hr />
-
-
-      <div className='flex'>
-        <Card name="Ali" age="22" role="Admin" />
-        <Card name="Ahmed" age="25" role="Editor" />
-        <Card name="Sara" age="21" role="User" />
-        <Card name="Usman" age="24" role="Moderator" />
-        <Card name="Ayesha" age="23" role="Admin" />
-        <Card name="Hassan" age="26" role="User" />
-        <Card name="Fatima" age="20" role="Editor" />
-        <Card name="Bilal" age="27" role="Admin" />
-        <Card name="Zain" age="22" role="User" />
-        <Card name="Mariam" age="24" role="Moderator" />
-      </div>
-
-      <br /> <hr />
-      <Users />
-
-    </div>
-  );
-}
-
-export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-import { Routes, Route } from "react-router-dom";
+import {  } from "./Components/Button";
+import { Routes, Route } from "react-router-dom"; 
 function App() {
   return (
     <div className="flex flex-col h-screen justify-between">
@@ -84,4 +27,8 @@ function App() {
 
         <Footer />
 
-    </div>*/
+    </div>
+  );
+}
+
+export default App;
